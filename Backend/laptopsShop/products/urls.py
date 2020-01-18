39f7af42d2b1c1cb701e25fa66363510b58db37d
InesIvanova <ines.iv.ivanova@gmail.com>
2020-01-18 21:40:45 +0200

    siple crud functionality of product

--- Backend/laptopsShop/products/urls.py
+++ Backend/laptopsShop/products/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 
 urlpatterns = [
     path('', views.ProductListView.as_view()),
+    re_path('^products/(?P<product_id>\d+)/$', views.ProductEditDelete.as_view()),
 ]
--- Backend/laptopsShop/products/views.py
+++ Backend/laptopsShop/products/views.py
@@ -12,9 +12,33 @@ class ProductListView(APIView):
         return Response(serializer.data)
 
     def post(self, request):
+        print(request.data)
         serializer = ProductSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductEditDelete(APIView):
+
+    def get(self, request, product_id):
+        product = self.get_object(pk=product_id)
+        serializer = ProductModel(product)
+        return Response(serializer.data)
+
+    def put(self, request, product_id):
+        product = self.get_object(pk=product_id)
+        serializer = ProductModel(product, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, product_id):
+        product = self.get_object(pk=product_id)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
